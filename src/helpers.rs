8d4784fd33582ec872df4f37466dbefe48e0055f
0xk1f0 <fteppan@protonmail.com>
2023-01-19 20:49:47 +0100

    basic functionality + add support for monitor input and offset

---
+++ src/helpers.rs
@@ -0,0 +1,72 @@
+use image::{GenericImageView, DynamicImage};
+use num::rational::Ratio;
+
+// simplify monitor ratio
+fn simplify_ratio(pixel1: u32, pixel2: u32) -> (u32, u32) {
+    let ratio = Ratio::new(pixel1, pixel2);
+    (*ratio.numer(), *ratio.denom())
+}
+
+// determine image ratio for offset
+fn determine_ratio(pixel1: u32, pixel2: u32) -> f64 {
+    let ratio = pixel1 as f64 / pixel2 as f64;
+    ratio
+}
+
+// parse cli input resolution
+pub fn parse_resolution(resolution: String) -> Result<(u32, u32), &'static str> {
+    let parts: Vec<&str> = resolution.split("x").collect();
+    println!("{}", parts[0]);
+    println!("{}", parts[1]);
+    let width = parts[0].parse::<u32>().map_err(|_| "")?;
+    let height = parts[1].parse::<u32>().map_err(|_| "")?;
+
+    Ok((width, height))
+}
+
+// split main image into two seperate
+pub fn split_image(input: &str, primary: &(u32, u32), secondary: &(u32, u32), offset: &u32) -> Result<Vec<DynamicImage>, &'static str> {
+    // new vector for result imgs
+    let mut result_papers: Vec<DynamicImage> = Vec::new();
+
+    println!("{}", input);
+    // open original input image
+    let mut img = image::open(input).map_err(|_| "")?;
+
+    // get base image dimensions
+    let (main_width, main_height) = img.dimensions();
+
+    // get monitor aspect ratios
+    let asp_primary = simplify_ratio(primary.0, primary.1);
+    let asp_secondary = simplify_ratio(secondary.0, secondary.1);
+
+    // ratio relations
+    let ratio_part_width = main_width / ( asp_primary.0 + asp_secondary.1 );
+    let ratio_part_height = main_height / asp_secondary.0;
+
+    // correctly size offset
+    let offset_ratiod: u32 = (*offset as f64 * determine_ratio(main_height, secondary.1)).ceil() as u32;
+
+    // Crop image for horizontal screen
+    let primary_img = img.crop(
+        ratio_part_width * asp_secondary.1,
+        offset_ratiod,
+        ratio_part_width * asp_primary.0,
+        ratio_part_height * asp_primary.1
+    );
+
+    // Crop image for vertical screen
+    let secondary_img = img.crop(
+        0,
+        0,
+        ratio_part_width * asp_secondary.1,
+        ratio_part_height * asp_secondary.0
+    );
+
+    // Push images to result vector
+    result_papers.push(primary_img);
+    result_papers.push(secondary_img);
+
+    // return the vector
+    Ok(result_papers)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,64 @@
+mod helpers;
 use std::error::Error;
-use image::{GenericImageView, DynamicImage};
+pub use helpers::split_image;
 
 pub struct Config {
     pub image_file: String,
-    pub offset: u32
+    pub m_primary: (u32,u32),
+    pub m_secondary: (u32,u32),
+    pub offset: u32,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
-        // handle arg count
-        if args.len() < 3 {
+        // handle args
+        if args.len() < 5 {
             return Err("not enough arguments");
-        } else if args.len() > 4 {
+        } else if args.len() > 6 {
             return Err("too many arguments");
-        } else if args[2].parse::<u32>().is_err() {
+        } else if args[4].parse::<u32>().is_err() {
             return Err("offset is non-int");
         }
 
+        // parse monitor vectors
+        let m_primary = helpers::parse_resolution(args[2].clone())
+            .map_err(
+                |_| "Invalid Primary Resolution"
+            )?;
+        let m_secondary = helpers::parse_resolution(args[3].clone())
+            .map_err(
+                |_| "Invalid Secondary Resolution"
+            )?;
+
         // clone args to vars
         let image_file: String = args[1].clone();
-        let offset: u32 = args[2].clone().trim().parse().unwrap();
+        let offset: u32 = args[4].clone()
+                                .trim()
+                                .parse()
+                                .unwrap();
 
+        // pass config
         Ok(Config {
             image_file,
+            m_primary,
+            m_secondary,
             offset
         })
     }
 }
 
-// determine image ratio for offset
-fn determine_ratio(pixel1: u32, pixel2: u32) -> f64 {
-    let ratio = pixel1 as f64 / pixel2 as f64;
-    ratio
-}
-
-// split main image into two seperate
-// @TODO: needs actual monitors sizes as input
-fn split_image(input: &str, offset: &u32) -> Vec<DynamicImage> {
-    let mut result_papers: Vec<DynamicImage> = Vec::new();
-    let mut img = image::open(input).unwrap();
-
-    // get base image dimensions
-    let (width, height) = img.dimensions();
-
-    // correctly size offset
-    let offset_ratiod: u32 = (*offset as f64 * determine_ratio(height, 1920)).ceil() as u32;
-    println!("{}", offset_ratiod);
-
-    // Crop image for horizontal screen
-    let horizontal_img = img.crop(
-        (width/25) * 9,
-        ((height/16) * 7) - offset_ratiod,
-        (width/25) * 16,
-        (height/16) * 9
-    );
-
-    // Crop image for vertical screen
-    let vertical_img = img.crop(
-        0,
-        0,
-        (width/25) * 9,
-        height
-    );
-
-    // Push images to result vector
-    result_papers.push(horizontal_img);
-    result_papers.push(vertical_img);
-
-    // return the vector
-    result_papers
-}
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // perform operation
+    let images = split_image(
+        &config.image_file,
+        &config.m_primary,
+        &config.m_secondary,
+        &config.offset
+    ).map_err(
+       |_| "Split Error"
+    )?;
 
-    let images = split_image(&config.image_file, &config.offset);
-
+    // export images
     for image in images {
         let image_name = format!(
             "out_img_{}x{}_{}.png",
@@ -85,5 +69,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         image.save(image_name).unwrap();
     }
 
+    // return none if success
     Ok(())
 }
