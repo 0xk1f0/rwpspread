f52a0dbfe46a9e23dd9acdc3d3a9dc8df669aadb
0xk1f0 <fteppan@protonmail.com>
2023-02-25 21:39:07 +0100

    another major code cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,26 @@ mod wpaperd;
 
 use std::process;
 use std::error::Error;
-use std::env::var;
 use colored::Colorize;
 use parser::Config;
-use splitter::split_image;
-use wpaperd::WpaperdConfig;
+use splitter::Splitter;
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // perform split operation
-    let result = split_image(
-        &config
-    ).map_err(
+    // create new splitter
+    let worker = Splitter::new(config);
+
+    // perform split
+    worker.run().map_err(
        |_| "error while splitting"
     )?;
-
-    // export images
-    for paper in &result {
-        let _ = paper.image.save(
-            &paper.image_full_path
-        ).map_err(
-            |_| "error while saving"
-        )?;
-    }
-
-    // check if we have to create a wpaperd config
-    if config.with_wpaperd {
-        let wpaperd = WpaperdConfig::new(
-            format!("{}/.config/wpaperd/output.conf", var("HOME").unwrap()),
-            result
-        );
-        wpaperd.build().map_err(
-            |_| "error while creating wpaperd config"
-        )?;
-    }
 
     // return
     Ok(())
 }
 
 fn main() {
-    // create new config
-    let config = Config::new();
-
-    // run splitter with config
-    if let Err(err) = run(config) {
+    // run with config
+    if let Err(err) = run(Config::new()) {
         eprintln!("{}: {}", "rwpspread".red().bold(), err);
         process::exit(1);
     }
--- src/splitter.rs
+++ src/splitter.rs
@@ -3,6 +3,8 @@ use std::cmp;
 use std::env::var;
 use colored::Colorize;
 use crate::Config;
+use crate::wpaperd::WpaperdConfig;
+use crate::layout::Monitor;
 
 // result paper struct
 pub struct ResultPaper {
@@ -11,87 +13,123 @@ pub struct ResultPaper {
     pub image: DynamicImage,
 }
 
-// split main image into two seperate, utilizes scaling
-pub fn split_image(config: &Config) -> Result<Vec<ResultPaper>, &'static str> {
-    // new vector for result imgs
-    let mut result_papers: Vec<ResultPaper> = Vec::new();
+pub struct Splitter {
+    base_image_path: String,
+    save_path: String,
+    monitors: Vec<Monitor>,
+    result_papers: Vec<ResultPaper>,
+    with_wpaperd: bool
+}
 
-    // open original input image
-    let mut img = image::open(&config.image_file).map_err(|_| "")?;
-
-    // get base image details
-    let (main_width, main_height) = img.dimensions();
-
-    /*
-        Calculate Overall Size
-        Assuming the following configuration:
-
-        +--------------+  +-----------------+
-        |              |  |                 |
-        |              |  |    primary      |
-        |              |  |     monitor     |
-        |  secondary   |  |                 |
-        |    monitor   |  +-----------------+
-        |              |
-        |              |
-        |              |
-        +--------------+
-        
-        Assuming a monitor can never be negatively offset
-        from root, we can say that max width will be the biggest monitor
-        with the greatest x-offset, max height will be defined in the same
-        way except using y-offset
-
-        Should we ever get a negative offset, this will definitely panic ¯\_(ツ)_/¯
-    */
-    let mut overall_width = 0;
-    let mut overall_height = 0;
-    for monitor in &config.mon_list {
-        overall_width = cmp::max(monitor.width + monitor.x as u32, overall_width);
-        overall_height = cmp::max(monitor.height + monitor.y as u32, overall_height);
+impl Splitter {
+    pub fn new(cfg: Config) -> Self {
+        Self {
+            base_image_path: cfg.image_file,
+            save_path: format!("{}/.cache/", var("Home").unwrap()),
+            monitors: cfg.mon_list,
+            result_papers: Vec::new(),
+            with_wpaperd: cfg.with_wpaperd
+        }
     }
+    // split main image into two seperate, utilizes scaling
+    pub fn run(mut self) -> Result<(), String> {
+        // open original input image
+        let mut img = image::open(&self.base_image_path).map_err(|_| "")?;
 
-    // check if we need to upscale
-    if overall_width > main_width || overall_height > main_height {
-        // we need to scale
-        println!(
-            "{}: Scaling image to fit {}x{}",
-            "WARNING".red().bold(),
-            overall_width,
-            overall_height
-        );
+        // get base image details
+        let (main_width, main_height) = img.dimensions();
 
-        // upscale image to fit
-        img = img.resize_to_fill(
-            overall_width,
-            overall_height,
-            FilterType::Lanczos3
-        );
+        /*
+            Calculate Overall Size
+            Assuming the following configuration:
+
+            +--------------+  +-----------------+
+            |              |  |                 |
+            |              |  |    primary      |
+            |              |  |     monitor     |
+            |  secondary   |  |                 |
+            |    monitor   |  +-----------------+
+            |              |
+            |              |
+            |              |
+            +--------------+
+            
+            Assuming a monitor can never be negatively offset
+            from root, we can say that max width will be the biggest monitor
+            with the greatest x-offset, max height will be defined in the same
+            way except using y-offset
+
+            Should we ever get a negative offset, this will definitely panic ¯\_(ツ)_/¯
+        */
+        let mut overall_width = 0;
+        let mut overall_height = 0;
+        for monitor in &self.monitors {
+            overall_width = cmp::max(monitor.width + monitor.x as u32, overall_width);
+            overall_height = cmp::max(monitor.height + monitor.y as u32, overall_height);
+        }
+
+        // check if we need to upscale
+        if overall_width > main_width || overall_height > main_height {
+            // we need to scale
+            println!(
+                "{}: Scaling image to fit {}x{}",
+                "WARNING".red().bold(),
+                overall_width,
+                overall_height
+            );
+
+            // upscale image to fit
+            img = img.resize_to_fill(
+                overall_width,
+                overall_height,
+                FilterType::Lanczos3
+            );
+        }
+
+        // Crop image for screens
+        for monitor in &self.monitors {
+            let cropped_image = img.crop(
+                monitor.x as u32,
+                monitor.y as u32,
+                monitor.width,
+                monitor.height
+            );
+            self.result_papers.push(
+                ResultPaper { 
+                    monitor_name: format!("{}", &monitor.name),
+                    image_full_path: format!(
+                        "{}rwpspread_{}_{}x{}.png",
+                        &self.save_path,
+                        format!("{}", &monitor.name),
+                        cropped_image.width(),
+                        cropped_image.height(),
+                    ),
+                    image: cropped_image
+                }
+            )
+        }
+
+        // save our result images
+        for paper in &self.result_papers {
+            paper.image.save(
+                &paper.image_full_path
+            ).map_err(
+                |_| "error while saving"
+            )?;
+        }
+
+        // check if we need to generate wpaperd config
+        if self.with_wpaperd {
+            let wpaperd = WpaperdConfig::new(
+                format!("{}/.config/wpaperd/output.conf", var("HOME").unwrap()),
+                self.result_papers
+            );
+            wpaperd.build().map_err(
+                |_| "error while creating wpaperd config"
+            )?;
+        }
+
+        // return
+        Ok(())
     }
-
-    // Crop image for screens
-    for monitor in &config.mon_list {
-        let cropped_image = img.crop(
-            monitor.x as u32,
-            monitor.y as u32,
-            monitor.width,
-            monitor.height
-        );
-        result_papers.push(
-            ResultPaper { 
-                monitor_name: format!("{}", &monitor.name),
-                image_full_path: format!(
-                    "{}/.cache/rwpspread_{}_{}x{}.png",
-                    var("HOME").unwrap(),
-                    format!("{}", &monitor.name),
-                    cropped_image.width(),
-                    cropped_image.height(),
-                ),
-                image: cropped_image
-            }
-        )
-    }
-
-    // return the vector
-    Ok(result_papers)
 }
