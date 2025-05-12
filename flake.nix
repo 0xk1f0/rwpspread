6ef1b593cf7ba654b552ebcbdbc8c442cc39a360
0xk1f0 <dev@k1f0.dev>
2025-05-12 19:32:40 +0200

    chore: fix flake.nix (forgot that I don't know nix)

--- flake.nix
+++ flake.nix
@@ -2,33 +2,34 @@
   description = "rwpspread: Multi-Monitor Wallpaper Spanning Utility";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { nixpkgs, ... }: let
-    pkgs = nixpkgs.legacyPackages;
+    pkgs-aarch64 = nixpkgs.legacyPackages.aarch64-linux;
+    pkgs-x86 = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    devShells.x86_64-linux.default = pkgs.x86_64-linux.mkShell {
+    devShells.x86_64-linux.default = pkgs-x86.mkShell {
       packages = builtins.attrValues {
-        inherit (pkgs) cargo rustfmt rust-analyzer pkg-config libxkbcommon;
+        inherit (pkgs-x86) cargo rustfmt rust-analyzer pkg-config libxkbcommon;
       };
     };
-    devShells.aarch64-linux.default = pkgs.aarch64-linux.mkShell {
+    devShells.aarch64-linux.default = pkgs-aarch64.mkShell {
       packages = builtins.attrValues {
-        inherit (pkgs) cargo rustfmt rust-analyzer pkg-config libxkbcommon;
+        inherit (pkgs-aarch64) cargo rustfmt rust-analyzer pkg-config libxkbcommon;
       };
     };
-    packages.x86_64-linux.default = pkgs.x86_64-linux.rustPlatform.buildRustPackage {
+    packages.x86_64-linux.default = pkgs-x86.rustPlatform.buildRustPackage {
       pname = "rwpspread";
       version = "master";
       src = ./.;
       cargoLock.lockFile = ./Cargo.lock;
-      nativeBuildInputs = [pkgs.pkg-config];
-      buildInputs = [pkgs.libxkbcommon];
+      nativeBuildInputs = [pkgs-x86.pkg-config];
+      buildInputs = [pkgs-x86.libxkbcommon];
     };
-    packages.aarch64-linux.default = pkgs.aarch64-linux.rustPlatform.buildRustPackage {
+    packages.aarch64-linux.default = pkgs-aarch64.rustPlatform.buildRustPackage {
       pname = "rwpspread";
       version = "master";
       src = ./.;
       cargoLock.lockFile = ./Cargo.lock;
-      nativeBuildInputs = [pkgs.pkg-config];
-      buildInputs = [pkgs.libxkbcommon];
+      nativeBuildInputs = [pkgs-aarch64.pkg-config];
+      buildInputs = [pkgs-aarch64.libxkbcommon];
     };
   };
 }
